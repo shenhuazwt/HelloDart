b70443a9401ddcc23688740a87770a7589633827
shenhuazwt <SHENHUAZWT@qq.com>
2022-03-31 08:40:38 +0000

    新建

--- 00_helloword.dart
+++ 00_helloword.dart
@@ -1,4 +1,20 @@
 //你好，世界，https://dart.cn/samples#hello-world
-void main(){
+void main() {
   print('Hello,World!');
+  varible();
 }
+
+//变量，https://dart.cn/samples#variables
+void varible(){
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter','Saturn','Unranus','Neptune'];
+  var image = {
+    'tags':['saturn'],
+    'url':'//path/saturn.jpg'
+  };
+  print('$name,$year,$antennaDiameter,$flybyObjects,$image');
+}
+
+//
