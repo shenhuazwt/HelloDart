09455e3bab34fc394ac3fa2779bf9157d3e7e781
shenhuazwt <SHENHUAZWT@qq.com>
2022-03-31 08:55:04 +0000

    新建控制语句

--- 00_helloword.dart
+++ 00_helloword.dart
@@ -5,24 +5,34 @@ void main() {
 }
 
 //变量，https://dart.cn/samples#variables
-void varible(){
+void varible() {
   var name = 'Voyager I';
   var year = 1977;
   var antennaDiameter = 3.7;
-  var flybyObjects = ['Jupiter','Saturn','Unranus','Neptune'];
+  var flybyObjects = ['Jupiter', 'Saturn', 'Unranus', 'Neptune'];
   var image = {
-    'tags':['saturn'],
-    'url':'//path/saturn.jpg'
+    'tags': ['saturn'],
+    'url': '//path/saturn.jpg'
   };
   print('$name,$year,$antennaDiameter,$flybyObjects,$image');
+  control_flow_statements(year, flybyObjects);
 }
 
-//流程控制语句 https://dart.cn/samples#control-flow-statements
-void control_flow_statements(){
-  if(year>=2001){
+//流程控制语句,https://dart.cn/samples#control-flow-statements
+void control_flow_statements(var year, var flybyObjects) {
+  if (year >= 2001) {
     print('21st century');
-  }else if(year>=1901) {
+  } else if (year >= 1901) {
     print('20th century');
   }
-  
+  for (final object in flybyObjects) {
+    print(object);
+  }
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
 }
