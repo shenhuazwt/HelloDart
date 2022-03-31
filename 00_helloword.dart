1a853eb637929d29cf8520fcf6735c8d7e85fae1
shenhuazwt <SHENHUAZWT@qq.com>
2022-03-31 09:23:36 +0000

    新建注释

--- 00_helloword.dart
+++ 00_helloword.dart
@@ -44,6 +44,19 @@ void control_flow_statements(var year, var flybyObjects) {
 //函数，https://dart.cn/samples#functions
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  return fibonacci(n - 1) + fibonacci(n - 2);//递归
 }
 //var result = fibonacci(20);在main函数里面调用
+
+//=> (胖箭头) 简写语法用于仅包含一条语句的函数。该语法在将匿名函数作为参数传递时非常有用
+//flybyObjects.where((name)=>name.contains('turn')).forEach(print);
+
+//注释https://dart.cn/samples#comments
+//常用//作为注释的开始
+///这是一个文档注释
+///文档注释用于库、类以及成员添加注释
+///像IDE和dartdoc这样的工具可以专门处理文档注释
+
+/*也可以像这样使用单斜杠和星号的注释方式*/
+
+//
